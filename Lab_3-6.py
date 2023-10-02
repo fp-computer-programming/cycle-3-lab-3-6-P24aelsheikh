0588aa80f8d1e45233a68b53f505fee14c5247d4
P24aelsheikh <144148097+P24aelsheikh@users.noreply.github.com>
2023-10-02 14:26:52 -0400

    Add files via upload

---
+++ Lab_3-6.py
@@ -0,0 +1,25 @@
+"""
+Create a Python file named lab_3-6.py
+
+Write a statement that creates the string "Fairfield Prep" by concatenating substrings.
+
+Write a statement that creates a dashed line that is 20 dashes long.
+
+Write a statement that prints the following:
+I'm loving this short story I'm reading, "The Fall of the House of Usher"
+
+Write a statement that prints the length of the previous string.
+
+Write a statement using the index operator that returns the "L" in "apple"
+"""
+
+x='Fairfield' + ' ' +  'Prep'
+print(x)
+x="-" * 20
+print(x)
+x="I'm loving this short story I'm reading,The Fall of the House of Usher"
+print(x)
+x="-" * 70
+print(x)
+x="apple"
+print(x[3]string)
